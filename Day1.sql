394f60065919c3c4c5e534f34d2a8befd4335281
Tejashwini Ganiger <tejashwiniganiger.xworkz@gmail.com>
2023-06-06 09:16:27 +0530

    files are added

---
+++ Day1.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE march_27th;
+use march_27th;
+
+CREATE DATABASE person_details;
+use person_details;
+
+CREATE DATABASE bank_details;
+use bank_details;
+
+CREATE DATABASE employee_details;
+use employee_details;
